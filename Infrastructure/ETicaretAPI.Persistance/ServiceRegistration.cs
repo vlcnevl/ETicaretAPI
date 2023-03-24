5079ff42163747ae49ed09975bb037bd16098d5e
Velican EVLİ <veli-can-evli06@hotmail.com>
2023-03-24 18:28:54 +0300

    base product and ıoc

---
+++ Infrastructure/ETicaretAPI.Persistance/ServiceRegistration.cs
@@ -0,0 +1,17 @@
+﻿using ETicaretAPI.Application.Abstractions;
+using ETicaretAPI.Persistance.Concretes;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace ETicaretAPI.Persistance
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistanceServices(this IServiceCollection services) // IoC ye eklemek için extension func yazdık değer. yapınca gelsin diye
+        {
+            services.AddSingleton<IProductService, ProductService>();
+        }
+
+
+    }
+}
