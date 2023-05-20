74a132c5809551260bcbad9184ba57a863993fca
Velican EVLİ <veli-can-evli06@hotmail.com>
2023-05-20 15:30:25 +0300

    code refactoring...

--- Core/ETicaretAPI.Application/Features/Commands/AppUser/GoogleLogin/GoogleLoginCommandHandler.cs
+++ Core/ETicaretAPI.Application/Features/Commands/AppUser/GoogleLogin/GoogleLoginCommandHandler.cs
@@ -1,7 +1,7 @@
-﻿using ETicaretAPI.Application.Abstraction.Token;
+﻿using ETicaretAPI.Application.Abstraction.GoogleLogin;
+using ETicaretAPI.Application.Abstraction.Token;
 using ETicaretAPI.Application.DTOs;
 using ETicaretAPI.Domain.Entities.Identity;
-using Google.Apis.Auth;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
 using System;
@@ -15,54 +15,56 @@ namespace ETicaretAPI.Application.Features.Commands.AppUser.GoogleLogin
     public class GoogleLoginCommandHandler : IRequestHandler<GoogleLoginCommandRequest, GoogleLoginCommandResponse>
     { // todo bu işlemi infranstucture da yapıp bir interface ile burda kullanmak daha doğru
 
-        readonly UserManager<Domain.Entities.Identity.AppUser> _userManager;
-        readonly ITokenHandler _tokenHandler;
-        public GoogleLoginCommandHandler(UserManager<Domain.Entities.Identity.AppUser> userManager, ITokenHandler tokenHandler)
+      
+        readonly IGoogleLogin _googleLogin;
+        public GoogleLoginCommandHandler(UserManager<Domain.Entities.Identity.AppUser> userManager, ITokenHandler tokenHandler, IGoogleLogin googleLogin)
         {
-            _userManager = userManager;
-            _tokenHandler = tokenHandler;
+
+            _googleLogin = googleLogin;
         }
 
         public async Task<GoogleLoginCommandResponse> Handle(GoogleLoginCommandRequest request, CancellationToken cancellationToken)
         {
-            var settings = new GoogleJsonWebSignature.ValidationSettings()
-            {
-                Audience = new List<string> { "59853966872-m1d7fjkbsc1m887ldu8evnni7759726p.apps.googleusercontent.com" }
-            };
+            //var settings = new GoogleJsonWebSignature.ValidationSettings()
+            //{
+            //    Audience = new List<string> { "59853966872-m1d7fjkbsc1m887ldu8evnni7759726p.apps.googleusercontent.com" }
+            //};
 
-            var payload = await GoogleJsonWebSignature.ValidateAsync(request.IdToken, settings);
+            //var payload = await GoogleJsonWebSignature.ValidateAsync(request.IdToken, settings);
 
-            var info = new UserLoginInfo(request.Provider, payload.Subject, request.Provider);
+            //var info = new UserLoginInfo(request.Provider, payload.Subject, request.Provider);
 
-            Domain.Entities.Identity.AppUser user = await _userManager.FindByLoginAsync(info.LoginProvider, info.ProviderKey);
+            //Domain.Entities.Identity.AppUser user = await _userManager.FindByLoginAsync(info.LoginProvider, info.ProviderKey);
 
-            bool result = user != null;
-            if (user == null)
-            {
-                user = await _userManager.FindByEmailAsync(payload.Email);
-                if (user == null)
-                {
-                    user = new()
-                    {
-                        Id = Guid.NewGuid().ToString(),
-                        Email = payload.Email,
-                        UserName = payload.Email,
-                        NameSurname = payload.Name,
-                    };
-                    var identityResult = await _userManager.CreateAsync(user); // asp.net users tablosuna kayıt
-                    result = identityResult.Succeeded;
+            //bool result = user != null;
+            //if (user == null)
+            //{
+            //    user = await _userManager.FindByEmailAsync(payload.Email);
+            //    if (user == null)
+            //    {
+            //        user = new()
+            //        {
+            //            Id = Guid.NewGuid().ToString(),
+            //            Email = payload.Email,
+            //            UserName = payload.Email,
+            //            NameSurname = payload.Name,
+            //        };
+            //        var identityResult = await _userManager.CreateAsync(user); // asp.net users tablosuna kayıt
+            //        result = identityResult.Succeeded;
 
-                }
-            }
+            //    }
+            //}
 
-            if (result)
-                await _userManager.AddLoginAsync(user, info);// Asp.NetUserLogins tablosuna eklendi.
+            //if (result)
+            //    await _userManager.AddLoginAsync(user, info);// Asp.NetUserLogins tablosuna eklendi.
 
-            else
-                throw new Exception("Google ile giriş işlemi başarısız.");
+            //else
+            //    throw new Exception("Google ile giriş işlemi başarısız.");
 
-          
-            Token token =  _tokenHandler.CreateAccessToken(5);
+
+            //Token token =  _tokenHandler.CreateAccessToken(5);
+
+            Token token = await _googleLogin.Login(request);
             return new()
             {
                 Token = token
